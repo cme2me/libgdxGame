607a8b6f385472b0ab3f4844fdb9f83c8d0ce84b
Max <max.zhakov@gmail.com>
2022-08-30 20:34:42 +0300

    camera

--- core/src/com/mygdx/game/screens/GameScreen.java
+++ core/src/com/mygdx/game/screens/GameScreen.java
@@ -5,7 +5,11 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.mygdx.game.Main;
 
@@ -13,9 +17,16 @@ public class GameScreen implements Screen {
     private final Main main;
     private OrthographicCamera camera;
     private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+    private final Batch batch;
+
     public GameScreen(Main game) {
         this.main = game;
-
+        TmxMapLoader tm = new TmxMapLoader();
+        map = tm.load("map/map.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch = new SpriteBatch();
     }
 
     @Override
@@ -26,6 +37,12 @@ public class GameScreen implements Screen {
     @Override
     public void render(float delta) {
         ScreenUtils.clear(Color.WHITE);
+        camera.update();
+        renderer.setView(camera);
+        renderer.render();
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        batch.end();
         if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
             main.dispose();
             main.setScreen(new MenuScreen(main));
